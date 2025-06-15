9dde720de5983cea8b505b1c1c1c2183f4f08824
Kris Kewish <kristopherkewish@gmail.com>
2025-06-15 16:14:52 +1000

    Test script to add todo

---
+++ script.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "./generated/prisma";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const todo = await prisma.todo.create({
+    data: {
+      title: "Buy groceries",
+    },
+  });
+  console.log(todo);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
