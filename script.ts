5904fdfcabdce2a19466b2cb35e3f6a14969c95d
Kris Kewish <kristopherkewish@gmail.com>
2025-06-15 16:27:20 +1000

    Add user model in schema and add todos

--- script.ts
+++ script.ts
@@ -3,8 +3,46 @@ import { PrismaClient } from "./generated/prisma";
 const prisma = new PrismaClient();
 
 async function main() {
-  const todo = await prisma.todo.findMany();
-  console.log(todo);
+  // Create a user and assign a todo to the user
+  const user1 = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "john@doe.com",
+      password: "123456",
+      todos: {
+        create: [
+          {
+            title: "Buy groceries",
+          },
+          {
+            title: "Pay bills",
+          },
+        ],
+      },
+    },
+  });
+
+  // Create a second user and assign a couple todos to the second user
+  const user2 = await prisma.user.create({
+    data: {
+      name: "Jane Doe",
+      email: "jane@doe.com",
+      password: "123456",
+      todos: {
+        create: [
+          {
+            title: "Buy fuel",
+          },
+          {
+            title: "Walk the dog",
+          },
+        ],
+      },
+    },
+  });
+
+  console.log("User 1:", user1);
+  console.log("User 2:", user2);
 }
 
 main()
