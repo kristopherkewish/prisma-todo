7740bae09e58b8a060b68f2c901d7f1508e0e389
Kris Kewish <kristopherkewish@gmail.com>
2025-06-15 16:35:41 +1000

    Create test script to load users with todos

--- script.ts
+++ script.ts
@@ -3,46 +3,13 @@ import { PrismaClient } from "./generated/prisma";
 const prisma = new PrismaClient();
 
 async function main() {
-  // Create a user and assign a todo to the user
-  const user1 = await prisma.user.create({
-    data: {
-      name: "John Doe",
-      email: "john@doe.com",
-      password: "123456",
-      todos: {
-        create: [
-          {
-            title: "Buy groceries",
-          },
-          {
-            title: "Pay bills",
-          },
-        ],
-      },
+  const users = await prisma.user.findMany({
+    include: {
+      todos: true,
     },
   });
 
-  // Create a second user and assign a couple todos to the second user
-  const user2 = await prisma.user.create({
-    data: {
-      name: "Jane Doe",
-      email: "jane@doe.com",
-      password: "123456",
-      todos: {
-        create: [
-          {
-            title: "Buy fuel",
-          },
-          {
-            title: "Walk the dog",
-          },
-        ],
-      },
-    },
-  });
-
-  console.log("User 1:", user1);
-  console.log("User 2:", user2);
+  console.dir(users, { depth: null });
 }
 
 main()
