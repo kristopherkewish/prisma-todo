646d3a34795b7cb268445ac07aca3eafeda3e877
Kris Kewish <kristopherkewish@gmail.com>
2025-06-16 18:05:05 +1000

    Formatting adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,38 @@
 import prisma from "@/lib/prisma";
 
 export default async function Home() {
-  const todos = await prisma.user.findMany({
+  const users = await prisma.user.findMany({
     include: {
       todos: true,
     },
   });
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">Todos</h1>
-      <ul className="flex flex-col gap-2">
-        {todos.map((user) => (
-          <li key={user.id} className="text-lg flex flex-col gap-2">
-            <h2 className="text-lg font-bold">{user.name}</h2>
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-2xl font-bold">Users</h1>
+      {users.map((user) => {
+        return (
+          <div
+            key={user.id}
+            className="flex flex-col gap-2 border-2 border-gray-300 p-4 rounded-md w-1/4"
+          >
+            <h2 className="text-lg font-bold bg-gray-200 p-2 rounded-md text-black">
+              {user.name}
+            </h2>
             <ul className="flex flex-col gap-2">
               {user.todos.map((todo) => (
                 <li key={todo.id} className="text-sm flex flex-col gap-2">
-                  <span className="font-bold">{todo.title}</span>
-                  <span className="text-sm">{todo.completed ? "Completed" : "Not Completed"}</span>
+                  <div className="flex justify-between">
+                    <span className="font-bold">{todo.title}</span>
+                    <span className="text-sm">
+                      {todo.completed ? "Completed" : "Not Completed"}
+                    </span>
+                  </div>
                 </li>
               ))}
             </ul>
-          </li>
-        ))}
-      </ul>
+          </div>
+        );
+      })}
     </div>
   );
 }
